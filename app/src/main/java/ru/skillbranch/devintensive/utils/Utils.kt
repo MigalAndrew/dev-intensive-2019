f1f5fb79dc2458072aa183eff357ff69b12e0e22
MigalAndrew <51985559+MigalAndrew@users.noreply.github.com>
2019-07-16 00:32:54 +0300

    hometask_2 complete

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -2,12 +2,19 @@ package ru.skillbranch.devintensive.utils
 
 object Utils {
     fun parseFullName(fullName:String?):Pair<String?, String?> {
-        //TODO FIX ME!!!
-        val parts: List<String>? = fullName?.split(" ")
 
-        val firstName = parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1)
-        return firstName to lastName
+        return when (fullName) {
+            null, "", " " -> Pair(null, null)
+            else -> {
+                val parts: List<String>? = fullName?.split(" ")
+
+                val firstName = parts?.getOrNull(0)
+                val lastName = parts?.getOrNull(1)
+                firstName to lastName
+            }
+        }
+
+
     }
 
     fun transliteration(payload: String, divider:String =" "): String {
