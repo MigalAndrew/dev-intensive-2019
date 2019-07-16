6d41bf1e914365d1e3d06992e09e75ea46243b4d
MigalAndrew <51985559+MigalAndrew@users.noreply.github.com>
2019-07-17 00:41:30 +0300

    hometask_2 complete

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -3,10 +3,10 @@ package ru.skillbranch.devintensive.utils
 object Utils {
     fun parseFullName(fullName:String?):Pair<String?, String?> {
 
-        return when (fullName) {
+        return when (fullName?.trim()) {
             null, "", " " -> Pair(null, null)
             else -> {
-                val parts: List<String>? = fullName?.split(" ")
+                val parts: List<String>? = fullName?.trim().split(" ")
 
                 val firstName = parts?.getOrNull(0)
                 val lastName = parts?.getOrNull(1)
@@ -23,7 +23,10 @@ object Utils {
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
-        //TODO("not impl") //to
-        return ""
+
+        val initials : String?
+        initials = firstName?.elementAtOrNull(0).toString()
+
+        return initials
     }
 }
