57653ed5f69fd630a412194af52805a9c62e576f
MigalAndrew <51985559+MigalAndrew@users.noreply.github.com>
2019-07-31 01:22:34 +0300

    hometask_3 complete

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -8,13 +8,13 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 fun Activity.hideKeyboard() {
-    //if (isKeyboardOpen()) {
+    if (isKeyboardOpen()) {
 
         val view = this.currentFocus
         if (view != null) {
             (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(view.windowToken,0)
         }
-    //}
+    }
 
 }
 
